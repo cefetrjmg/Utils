f3fe1603eb16eb92b931aa24f33d47a2840607f6
cristian <cristianmadeira1106@gmail.com>
2019-09-18 12:38:52 -0300

    Primeiro commit da biblioteca.

---
+++ Utils/src/br/cefetrj/mg/bsi/utils/Utils.java
@@ -0,0 +1,45 @@
+package br.cefetrj.mg.bsi.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JOptionPane;
+
+public class Utils {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static String dateToText(Date d) {
+        return sdf.format(d);
+    }
+
+    public static Date textToDate(String d) throws ParseException {
+        return sdf.parse(d);
+    }
+
+    public static String input(String msg) {
+        return JOptionPane.showInputDialog(null, msg);
+    }
+
+    public static String input(String msg, String title) {
+        return JOptionPane.showInputDialog(null, msg, title);
+    }
+
+    public static void print(String msg) {
+        JOptionPane.showMessageDialog(null, msg);
+    }
+
+    public static void print(String msg, String title) {
+        JOptionPane.showMessageDialog(null, msg, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void print(String msg, String title, boolean error) {
+        JOptionPane.showMessageDialog(null, msg, title, (error ? JOptionPane.ERROR_MESSAGE : JOptionPane.INFORMATION_MESSAGE));
+    }
+
+    public static int confirm(String msg, String title) {
+        return JOptionPane.showConfirmDialog(null, msg, title, JOptionPane.YES_NO_OPTION);
+    }
+    
+
+}
